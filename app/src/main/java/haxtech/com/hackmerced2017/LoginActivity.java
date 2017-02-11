f1ac7a2adad6463e0f5b4439363d2e4e3fb925f6
Mohammad <momosharif58@gmail.com>
2017-02-11 02:14:29 -0800

    Added Login UI Buttons

--- app/src/main/java/haxtech/com/hackmerced2017/LoginActivity.java
+++ app/src/main/java/haxtech/com/hackmerced2017/LoginActivity.java
@@ -1,13 +1,65 @@
 package haxtech.com.hackmerced2017;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null){
+            // User has already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+        } else {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setProviders(
+                    AuthUI.FACEBOOK_PROVIDER,
+                    AuthUI.EMAIL_PROVIDER,
+                    AuthUI.GOOGLE_PROVIDER
+            ).build(), RC_SIGN_IN);
+        }
+
+        findViewById(R.id.log_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                // user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            } else {
+                //user not authenticated
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.log_out_button){
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Log.d("AUTH", "USER HAS LOGGED OUT");
+                    finish();
+                }
+            });
+        }
     }
 }

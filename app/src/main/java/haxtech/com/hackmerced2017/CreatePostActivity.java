21122e4f1a428951ac446ea95ea877691bdf8a35
Arjun Nayak <arjunglobal09@gmail.com>
2017-02-11 15:16:20 -0800

    creates posts and updates users array of postids

--- app/src/main/java/haxtech/com/hackmerced2017/CreatePostActivity.java
+++ app/src/main/java/haxtech/com/hackmerced2017/CreatePostActivity.java
@@ -5,17 +5,31 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CreatePostActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth auth;
 
 //    private EditText postTitle;
     private DatabaseReference mDatabase;
@@ -30,6 +44,8 @@ public class CreatePostActivity extends AppCompatActivity implements View.OnClic
         findViewById(R.id.postButton).setOnClickListener(this);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -40,15 +56,15 @@ public class CreatePostActivity extends AppCompatActivity implements View.OnClic
     }
 
     private void writeNewPost(String body) {
-        // Create new post at /user-posts/$userid/$postid and at
-        // /posts/$postid simultaneously
+        FirebaseUser curUser = auth.getCurrentUser();
         String key = mDatabase.child("posts").push().getKey();
-        String userID = "1";
-        Post post = new Post("101", userID, body);
+        String userID = curUser.getUid();
+        Post post = new Post(userID, body, "https://testurl.com/12345.mp4");
         Map<String, Object> postValues = post.toMap();
         Map<String, Object> childUpdates = new HashMap<>();
         childUpdates.put("/posts/" + key, postValues);
-        childUpdates.put("/user-posts/" + userID + "/" + key, postValues);
-        mDatabase.updateChildren(childUpdates);
+        DatabaseReference listOfPostsRef = mDatabase.child("users").child(userID).child("posts");
+        listOfPostsRef.push().setValue(key);
     }
 }
+
--- app/src/main/java/haxtech/com/hackmerced2017/Post.java
+++ app/src/main/java/haxtech/com/hackmerced2017/Post.java
@@ -9,26 +9,29 @@ import java.util.Map;
 @IgnoreExtraProperties
 public class Post {
 
-    public String uid;
     public String userID;
     public String body;
+    public double avgRating;
+    public String vidUrl;
 
     public Post() {
         // Default constructor required for calls to DataSnapshot.getValue(Post.class)
     }
 
-    public Post(String uid, String userID, String body) {
-        this.uid = uid;
+    public Post(String userID, String body, String vidUrl) {
         this.userID = userID;
         this.body = body;
+        this.vidUrl = vidUrl;
+        this.avgRating = 0.0;
     }
 
     @Exclude
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("uid", uid);
         result.put("userID", userID);
         result.put("body", body);
+        result.put("avgRating", avgRating);
+        result.put("vidUrl", vidUrl);
         return result;
     }
 
